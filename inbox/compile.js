af620947d23e4c49ba4ee2813e2b222b0d09b1f0
Raghvendra Jain <raghavendra.jain@gmail.com>
2019-05-29 13:17:12 +0900

    Code to compile the contract

---
+++ inbox/compile.js
@@ -0,0 +1,9 @@
+const path = require("path"); //cross platform compatibility is provided by the this module for paths
+const fs = require("fs"); // 
+const solc = require("solc");
+
+const inboxPath = path.resolve(__dirname, "contracts", "inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
+console.log(solc.compile(source, 1));
+
+
