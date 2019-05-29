31c4c3676d6dca8cd9916a6a3d5a113da99d7a01
Raghvendra Jain <raghavendra.jain@gmail.com>
2019-05-29 15:31:11 +0900

    some changes

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,7 +1,9 @@
 const assert  = require("assert"); // to test whether someone value is equal to another value
 const ganache = require("ganache-cli");
 const Web3    = require("web3"); // we are importing a constructor function, thats why it is W in Web3 and not web3, so by convention whenever we use a constructor function, we capitalize it
-const provider= ganache.provider();
+const provider= ganache.provider(); // we can have providers that communicate via web sockets, 
+                                   // via http and also via IPC. Ganache uses IPC. 
+                                   // any network that runs locally in your PC, uses inter process communication 
 const web3    = new Web3(provider); // an instance of Web3. The provider is a communication layer between the blockchain network and the web3 library. The provider comes from the network itself. 
 const {interface, bytecode} = require("../compile"); // 
 
